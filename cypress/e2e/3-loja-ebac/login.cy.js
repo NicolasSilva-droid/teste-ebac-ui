ff5bfaee2f4de5d504becf4cdaeabb0a881d5276
Nicolas Silva <nicolas19388108@gmail.com>
2025-09-19 19:27:54 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,13 +2,30 @@
 
 describe('Funcionalidade Login', () => {
     
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+    
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('nicolas.teste@teste.com.br')
         cy.get('#password').type('teste123')
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, nicolas.teste (não é nicolas.teste? Sair)')
     })
+
+    it('Deve exibir uma mensagem de erro ao inserir um usuário inválido', () => {
+        
+        cy.get('#username').type('sandro@teste.com.br')
+        cy.get('#password').type('teste123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('  .woocommerce-error').should('contain' , 'Endereço de e-mail desconhecido.')
+        cy.get('  .woocommerce-error').should('exist')
+    });
 
 })
